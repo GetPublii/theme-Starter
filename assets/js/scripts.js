635aa31ae1df9508e9f34af781a5390aba902db9
Tomasz Dziuda <dziudek@gmail.com>
2017-08-13 11:55:51 +0200

    Small code improvements

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
-(function ($) {
-    // Dropdown menu
-    $(function () {
+(function($) {
+    /**
+     * Dropdown menu
+     */
+    $(function() {
         $('.js-navbar__toggle').on('click', function () {
             $('.js-navbar').toggleClass('is-opened');
             $('.js-navbar__toggle').attr('aria-expanded', $('.js-navbar').hasClass('is-opened'));
@@ -23,10 +25,14 @@
         });
     });
 
-    // iOS :hover fix
+    /**
+     * iOS :hover fix
+     */
     document.addEventListener("touchend", function() {});
 
-    // EU Cookie Law Bar
+    /**
+     * EU Cookie Law Bar
+     */
     $(window).on("load", function () {
         var cookieBar = $('.js-cookie-bar');
 
@@ -45,21 +51,25 @@
         if (!localStorage.getItem('themes-cookie-bar')) {
             cookieBar.addClass('is-sticky');
         }
-    });    
-  
-    // Share buttons pop-up
-    $(function () {
+    });
+
+    /**
+     * Share buttons pop-up
+     */
+    $(function() {
         // link selector and pop-up window size
         var Config = {
             Link: ".js-share",
             Width: 500,
             Height: 500
         };
+
         // add handler links
         var slink = document.querySelectorAll(Config.Link);
         for (var a = 0; a < slink.length; a++) {
             slink[a].onclick = PopupHandler;
         }
+
         // create popup
         function PopupHandler(e) {
             e = (e ? e : window.event);
@@ -82,5 +92,4 @@
             return !!popup;
         }
     });
-   
 })(jQuery);
--- visual-override.js
+++ visual-override.js
@@ -2,192 +2,193 @@
  * Custom function used to generate the output of the override.css file
  */
 
-var generateOverride = function (params) {   
+var generateOverride = function (params) {
     let output = '';
 
-if(params.minFontSize !== '1' || params.maxFontSize !== '1.3') {
+    if(params.minFontSize !== '1' || params.maxFontSize !== '1.3') {
         output += `
-html {
-  font-size: ${params.minFontSize}rem;
-}
+        html {
+          font-size: ${params.minFontSize}rem;
+        }
 
-@media screen and (min-width: 20rem) {
-  html {
-       font-size: calc(${params.minFontSize}rem + (${params.maxFontSize} - ${params.minFontSize}) * ((100vw - 20rem) / 113));
-  }
-}
+        @media screen and (min-width: 20rem) {
+          html {
+               font-size: calc(${params.minFontSize}rem + (${params.maxFontSize} - ${params.minFontSize}) * ((100vw - 20rem) / 113));
+          }
+        }
 
-@media screen and (min-width: 133rem) {
-  html { 
-       font-size: ${params.maxFontSize}rem;
+        @media screen and (min-width: 133rem) {
+          html {
+               font-size: ${params.maxFontSize}rem;
+            }
+        }`;
     }
-  }`;    	 
-}
 
-if(params.textColor !== '#343434') {
+    if(params.textColor !== '#343434') {
         output += `
-body {
-       color: ${params.textColor};  
-  }`;   
-}
-	
-if(params.headingColor !== '#343434') {
-        output += `
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-       color: ${params.headingColor};           
-  }`;    	 
-}
-	
-if(params.linkColor !== '#343434') {
-        output += `
-a {
-       color: ${params.linkColor}; 
-}
-.post__entry a:hover,
-.post__entry a:active,
-.post__entry a:focus {
-       color: ${params.linkColor}; 
-  }`;    	 
-}	
-	
-if(params.linkHoverColor !== '#3949ab') {
-        output += `
-a:hover,
-a:active,
-a:focus {
-       color: ${params.linkHoverColor};
-}
-.post__entry a {
-       color: ${params.linkHoverColor};
-}
-.post__nav__link:hover h5 {
-       color: ${params.linkHoverColor};
-  }`;    	 
-}	
-	
-if(params.primaryColor !== '#3949ab') {
-        output += `      
-.btn,
-[type=button],
-[type=submit],
-button {
-       background: ${params.primaryColor};           
-  }`;    	 
-}
-	
-if(params.primaryColor !== '#3949ab') {
-        output += `      
-[type=text]:focus,
-[type=url]:focus,
-[type=tel]:focus,
-[type=number]:focus,
-[type=email]:focus,
-[type=search]:focus,
-textarea:focus,
-select:focus {
-       -webkit-box-shadow: inset 0 0 2px ${params.primaryColor};
-       box-shadow: inset 0 0 2px ${params.primaryColor};           
-  }`;    	 
-}
-		
-if(params.primaryColor !== '#3949ab') {
-        output += `
-[type=checkbox]:checked+.checkbox:before {
-       color: ${params.primaryColor};           
-  }`;    	 
-}
-	
-if(params.primaryColor !== '#3949ab') {
-        output += `
-[type=radio]:checked+.radio:before {
-       background: ${params.primaryColor};           
-  }`;    	 
-}
-
-if(params.primaryColor !== '#3949ab') {
-        output += `
-select[multiple]:focus {
-       border-color: ${params.primaryColor};           
-  }`;    	 
-}
-	
-if(params.primaryColor !== '#3949ab') {
-        output += `
-.box__title {
-       color: ${params.primaryColor};           
-  }`;    	 
-}
-	
-if(params.primaryColor !== '#3949ab') {
-        output += `
-.page-header__title {
-       color: ${params.primaryColor};           
-  }`;    	 
-}
-
-if(params.mainWidth !== '42rem' || params.sidebarWidth !== '20rem') {
-        output += `   
-@media all and (min-width: 56.25em) {
-.container {
-       grid-template-columns: 1fr minmax(auto, ${params.mainWidth}) ${params.sidebarWidth} 1fr;
-	}            
-  }`;    	 
-}	
-	
-if(params.submenu === 'custom') {
-        output += `
-@media all and (min-width: 56.25em) {
-.navbar__submenu  {
-       width: ${params.submenuWidth}rem;
+        body {
+               color: ${params.textColor};
+        }`;
     }
-  }`;
-}
-if(params.linkColorMenu !== '#343434') {
-        output += `
-.navbar__menu li a,
-.navbar__menu li span {
-       color: ${params.linkColorMenu}; 
-  }`;    	 
-}
-	
-if(params.linkHoverColorMenu !== '#3949ab') {
-        output += `
-.navbar__menu li a:hover:not(span),
-.navbar__menu li span:hover:not(span) {
-       color: ${params.linkHoverColorMenu}; 
-  }`;    	 
-}	
-	
-if(params.heightHero !== '29rem') {
-        output += ` 
-.hero { 
-       min-height: ${params.heightHero}
-}
-.hero__image { 
-       min-height: ${params.heightHero}
-  }`;    	 
-}
 
-if(params.headingHeroColor !== '#ffffff') {
-        output += `   
-.hero__text>h1 {
-        color: ${params.headingHeroColor}; 
-  }`;    	 
-}
-	
-if(params.textHeroColor !== '#ffffff') {
-        output += `   
-.hero__text {
-       color: ${params.textHeroColor}; 
-  }`;    	 
-}
+    if(params.headingColor !== '#343434') {
+        output += `
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+               color: ${params.headingColor};
+        }`;
+    }
 
- return output;
+    if(params.linkColor !== '#343434') {
+        output += `
+        a {
+               color: ${params.linkColor};
+        }
+        .post__entry a:hover,
+        .post__entry a:active,
+        .post__entry a:focus {
+               color: ${params.linkColor};
+        }`;
+    }
+
+    if(params.linkHoverColor !== '#3949ab') {
+        output += `
+        a:hover,
+        a:active,
+        a:focus {
+               color: ${params.linkHoverColor};
+        }
+        .post__entry a {
+               color: ${params.linkHoverColor};
+        }
+        .post__nav__link:hover h5 {
+               color: ${params.linkHoverColor};
+        }`;
+    }
+
+    if(params.primaryColor !== '#3949ab') {
+        output += `
+        .btn,
+        [type=button],
+        [type=submit],
+        button {
+               background: ${params.primaryColor};
+        }`;
+    }
+
+    if(params.primaryColor !== '#3949ab') {
+        output += `
+        [type=text]:focus,
+        [type=url]:focus,
+        [type=tel]:focus,
+        [type=number]:focus,
+        [type=email]:focus,
+        [type=search]:focus,
+        textarea:focus,
+        select:focus {
+               -webkit-box-shadow: inset 0 0 2px ${params.primaryColor};
+               box-shadow: inset 0 0 2px ${params.primaryColor};
+        }`;
+    }
+
+    if(params.primaryColor !== '#3949ab') {
+        output += `
+        [type=checkbox]:checked+.checkbox:before {
+               color: ${params.primaryColor};
+        }`;
+    }
+
+    if(params.primaryColor !== '#3949ab') {
+        output += `
+        [type=radio]:checked + .radio:before {
+               background: ${params.primaryColor};
+        }`;
+    }
+
+    if(params.primaryColor !== '#3949ab') {
+        output += `
+        select[multiple]:focus {
+               border-color: ${params.primaryColor};
+        }`;
+    }
+
+    if(params.primaryColor !== '#3949ab') {
+        output += `
+        .box__title {
+               color: ${params.primaryColor};
+        }`;
+    }
+
+    if(params.primaryColor !== '#3949ab') {
+        output += `
+        .page-header__title {
+               color: ${params.primaryColor};
+        }`;
+    }
+
+    if(params.mainWidth !== '42rem' || params.sidebarWidth !== '20rem') {
+        output += `
+        @media all and (min-width: 56.25em) {
+        .container {
+               grid-template-columns: 1fr minmax(auto, ${params.mainWidth}) ${params.sidebarWidth} 1fr;
+            }
+        }`;
+    }
+
+    if(params.submenu === 'custom') {
+        output += `
+        @media all and (min-width: 56.25em) {
+        .navbar__submenu  {
+               width: ${params.submenuWidth}rem;
+            }
+        }`;
+    }
+
+    if(params.linkColorMenu !== '#343434') {
+        output += `
+        .navbar__menu li a,
+        .navbar__menu li span {
+               color: ${params.linkColorMenu};
+        }`;
+    }
+
+    if(params.linkHoverColorMenu !== '#3949ab') {
+        output += `
+        .navbar__menu li a:hover:not(span),
+        .navbar__menu li span:hover:not(span) {
+               color: ${params.linkHoverColorMenu};
+        }`;
+    }
+
+    if(params.heightHero !== '29rem') {
+        output += `
+        .hero {
+               min-height: ${params.heightHero}
+        }
+        .hero__image {
+               min-height: ${params.heightHero}
+        }`;
+    }
+
+    if(params.headingHeroColor !== '#ffffff') {
+        output += `
+        .hero__text > h1 {
+                color: ${params.headingHeroColor};
+        }`;
+    }
+
+    if(params.textHeroColor !== '#ffffff') {
+        output += `
+        .hero__text {
+               color: ${params.textHeroColor};
+        }`;
+    }
+
+    return output;
 }
 
 module.exports = generateOverride;
